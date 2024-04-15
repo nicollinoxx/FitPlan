5de6b7db4a14be0209f5600ae9a32ce783975f5e
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2024-04-14 22:47:51 -0300

    Add videos to workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -65,6 +65,6 @@ class WorkoutsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def workout_params
-      params.require(:workout).permit(:exercise, :series, :repetitions, :charge, :interval)
+      params.require(:workout).permit(:exercise, :series, :repetitions, :charge, :interval, :video)
     end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,6 @@
 class Workout < ApplicationRecord
+  has_one_attached :video
+
   validates :exercise, :series, :repetitions, presence: true
   validates :interval, :repetitions, length: { maximum: 15 }
 end
