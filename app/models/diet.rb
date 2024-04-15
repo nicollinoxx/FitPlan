55f7d15e9f1210037db51b3e2538ba609b673143
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2024-04-14 21:23:48 -0300

    Add Action_text to diets

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,10 +1,9 @@
 class Diet < ApplicationRecord
+  has_rich_text :description
+  
   validates :snack, :description, presence: true
 
-
-
   before_save :calculate_calories
-
   def calculate_calories
     self.calories = (protein_g * 4) + (carbohydrate_g * 4) + (fat_kcal = fat_g * 9)
   end
