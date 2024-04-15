a69b5433aeb86461ba173b3440dcd7f3cf5ebe6b
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2024-04-14 21:47:00 -0300

    Add correct calorie calculation

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,10 +1,9 @@
 class Diet < ApplicationRecord
   has_rich_text :description
-  
   validates :snack, :description, presence: true
 
   before_save :calculate_calories
   def calculate_calories
-    self.calories = (protein_g * 4) + (carbohydrate_g * 4) + (fat_kcal = fat_g * 9)
+    self.calories = (protein_g.to_f * 4) + (carbohydrate_g.to_f * 4) + (fat_g.to_f * 9)
   end
 end
