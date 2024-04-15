07c157887098e8192de3d3915043624e082b66b1
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2024-04-14 21:17:05 -0300

    Add simple calorie calculation

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,3 +1,11 @@
 class Diet < ApplicationRecord
   validates :snack, :description, presence: true
+
+
+
+  before_save :calculate_calories
+
+  def calculate_calories
+    self.calories = (protein_g * 4) + (carbohydrate_g * 4) + (fat_kcal = fat_g * 9)
+  end
 end
