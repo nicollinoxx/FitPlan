d552b1251ae10460d76ca7df85ba37e3ca1655c5
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2024-04-18 13:15:10 -0300

    Add validates to videos

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,5 +3,14 @@ class Workout < ApplicationRecord
   has_one_attached :video
 
   validates :exercise, :series, :repetitions, presence: true
-  validates :interval, length: { maximum: 15 }
+  validates :interval, length: { maximum: 15 }, allow_blank: true
+  validate :video_content_type
+
+   private
+
+   def video_content_type
+     if video.attached? && !video.content_type.in?(%w(video/mp4 video/mpeg video/quicktime))
+       errors.add(:video, 'must be a file (MP4, MPEG, QuickTime)')
+     end
+   end
 end
