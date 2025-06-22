1852045bbfce51e507fc49f5041f55118c6a2686
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2025-06-21 23:34:54 -0300

    Add theme toggle controller with localStorage persistence

---
+++ app/javascript/controllers/theme_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="theme"
+export default class extends Controller {
+  static targets = ["icon"]
+
+  connect() {
+    const theme = this.getlocalStorage()
+    document.documentElement.setAttribute("data-bs-theme", theme), this.changeIcon(theme)
+  }
+
+  applyTheme() {
+    const theme = this.getlocalStorage() === "light" ? "dark" : "light"
+    document.documentElement.setAttribute("data-bs-theme", theme), this.setLocalStorage(theme)
+    this.changeIcon(theme)
+  }
+
+  changeIcon(theme) {
+    this.iconTarget.classList.toggle("bi-sun-fill", theme === "light")
+    this.iconTarget.classList.toggle("bi-moon-stars-fill", theme === "dark")
+  }
+
+  getlocalStorage() {return localStorage.getItem("theme") || "light"}
+  setLocalStorage(theme) {localStorage.setItem("theme", theme)}
+}
