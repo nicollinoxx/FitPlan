71755e110d8ae347b9c6f6231b731f447d3b0e8a
nicollinoxx <nicolasalmeida321@gmail.com>
2025-07-06 18:42:19 -0300

    Retry jobs on StandardError

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,5 @@
 class ApplicationJob < ActiveJob::Base
+  retry_on StandardError, wait: :exponentially_longer, attempts: 3
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
