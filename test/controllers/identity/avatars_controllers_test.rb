6e9b884088e0e453ae9aff8285c69678244bf414
Nícolas Amaral <nicolasalmeida321@gmail.com>
2025-08-19 14:25:00 -0300

    Fix avatar form input and params to use user[avatar]

--- test/controllers/identity/avatars_controllers_test.rb
+++ test/controllers/identity/avatars_controllers_test.rb
@@ -6,26 +6,17 @@ class Identity::AvatarsControllerTest < ActionDispatch::IntegrationTest
     sign_in_as(@user)
   end
 
-  test "should get show and render current avatar if attached" do
-    @user.avatar.attach(io: file_fixture("avatar.png").open, filename: "avatar.png", content_type: "image/png")
-
-    get identity_avatar_url(@user)
-    assert_response :success
-    assert_select "img[src*='#{@user.avatar.filename}']"
-    assert_select "form[action='#{identity_avatar_path(@user)}'][method='post']"
-  end
-
   test "should get edit page with preview elements" do
     get edit_identity_avatar_url(@user)
     assert_response :success
     assert_select "form[action='#{identity_avatar_path}']"
-    assert_select "input[type=file][name='avatar']"
+    assert_select "input[type=file][name='user[avatar]']"
   end
 
   test "should update avatar" do
     file = fixture_file_upload("avatar.png", "image/png")
 
-    patch identity_avatar_url(@user), params: { avatar: file }
+    patch identity_avatar_url(@user), params: { user: { avatar: file } }
     assert_redirected_to account_url
     follow_redirect!
     assert_match I18n.t("notice.avatar.update"), response.body
