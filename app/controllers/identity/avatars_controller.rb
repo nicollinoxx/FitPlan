4d781eff7c7479f1deb34a673ccdbf1ee25098a1
Nícolas Amaral <nicolasalmeida321@gmail.com>
2025-08-21 22:24:09 -0300

    Refactor avatar handling and add form partial

--- app/controllers/identity/avatars_controller.rb
+++ app/controllers/identity/avatars_controller.rb
@@ -1,11 +1,8 @@
 class Identity::AvatarsController < ApplicationController
   before_action :set_user
 
-  def edit
-  end
-
   def update
-    @user.update!(user_params)
+    @user.update!(params.permit(:avatar))
     recede_or_redirect_to account_path, notice: I18n.t('notice.avatar.update')
   end
 
@@ -15,11 +12,8 @@ class Identity::AvatarsController < ApplicationController
   end
 
   private
+
     def set_user
       @user = Current.user
-    end
-
-    def user_params
-      params.expect(user: [:avatar])
     end
 end
