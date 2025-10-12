98b35ff638bf32b94155f5588ae7ffc6b48298bb
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2025-10-12 16:06:51 -0300

    Update 20250601134735_add_copy_to_sheets.rb

--- db/migrate/20250601134735_add_copy_to_sheets.rb
+++ db/migrate/20250601134735_add_copy_to_sheets.rb
@@ -1,5 +1,5 @@
 class AddCopyToSheets < ActiveRecord::Migration[8.0]
   def change
-    add_column :sheets, :copy, :boolean
+    add_column :sheets, :copy, :boolean, default: false, null: false
   end
 end
