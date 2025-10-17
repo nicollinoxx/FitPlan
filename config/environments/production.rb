20406e978106f364a5e8e6da1cc03c24554cfadc
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2025-10-17 00:24:03 -0300

    Configure SMTP in production
    Enable SMTP delivery and configure mailer settings in production. Use
    smtp.mailersend.net on port 587 with STARTTLS Retrieve user_name and
    password from Rails credentials[:smtp] Add smtp section to credentials
    to hold the credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "example.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    address: "smtp.mailersend.net",
+    port: 587,
+    starttls: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
