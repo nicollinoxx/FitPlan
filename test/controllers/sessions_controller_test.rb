23e1d37e3d6328b14e252cef2bd9789643ac8209
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2025-11-09 14:25:31 -0300

    Update session tests and add welcome test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -26,11 +26,12 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
   test "should not sign in with wrong credentials" do
     post sign_in_url, params: { email: @user.email, password: "SecretWrong1*3" }
+
     assert_redirected_to sign_in_url(email_hint: @user.email)
     assert_equal "Email or password is incorrect", flash[:notice]
 
-    get root_url
-    assert_redirected_to sign_in_url
+    follow_redirect!
+    assert_response :success
   end
 
   test "should sign out" do
@@ -40,6 +41,6 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to sessions_url
 
     follow_redirect!
-    assert_redirected_to sign_in_url
+    assert_redirected_to welcome_url
   end
 end
