e1adb169363d22e6650394ac38b018e01896dba8
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-01-24 23:51:26 -0300

    Use time_field for interval and remove length limits

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,5 +3,4 @@ class Workout < ApplicationRecord
   has_one_attached :video
 
   validates :exercise, :series, :repetitions, presence: true
-  validates :interval, :repetitions, length: { maximum: 15 }
 end
--- db/migrate/20240415005320_create_workouts.rb
+++ db/migrate/20240415005320_create_workouts.rb
@@ -11,12 +11,3 @@ class CreateWorkouts < ActiveRecord::Migration[7.1]
     end
   end
 end
-
-t.string "exercise"
-t.integer "series"
-t.string "repetitions"
-t.string "charge"
-t.datetime "created_at", null: false
-t.datetime "updated_at", null: false
-t.string "interval"
-t.integer "sheet_id", null: false
