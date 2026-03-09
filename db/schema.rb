9be3c463233a93528b221374091444a93546e930
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-03-08 22:56:42 -0300

    Add visibility to sheets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_26_011934) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_09_015502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -106,6 +106,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_26_011934) do
     t.string "sheet_type"
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.string "visibility", default: "shareable", null: false
     t.index ["user_id"], name: "index_sheets_on_user_id"
   end
 
