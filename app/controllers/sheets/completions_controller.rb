6420b228067d0dae5a0885c68f33921991c14daf
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-04-12 20:21:17 -0300

    Add warning pulse and trigger pulse on completion

--- app/controllers/sheets/completions_controller.rb
+++ app/controllers/sheets/completions_controller.rb
@@ -1,22 +1,15 @@
 module Sheets
   class CompletionsController < ApplicationController
     include SheetScoped
-    include ActionView::RecordIdentifier
 
     def create
       @sheet.complete!
-      refresh_sheet_card
+      render turbo_stream: turbo_stream.replace("sheet_#{@sheet.id}", partial: 'sheets/sheet', locals: { sheet: @sheet, pulse: true })
     end
 
     def destroy
       @sheet.uncomplete!
-      refresh_sheet_card
-    end
-
-    private
-
-    def refresh_sheet_card
-      render turbo_stream: turbo_stream.replace("#{dom_id(@sheet)}", partial: 'sheets/sheet', locals: { sheet: @sheet })
+      render turbo_stream: turbo_stream.replace("sheet_#{@sheet.id}", partial: 'sheets/sheet', locals: { sheet: @sheet, pulse: true })
     end
   end
 end
