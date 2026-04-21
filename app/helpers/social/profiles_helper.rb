1927eb6d9acdefe5f34ca716f4c06d7781e3a63e
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-04-21 18:53:12 -0300

    Tweak profile UI and add follow translations
    Use a secondary unfollow button and increase profile card padding.
    Add 'follow'/'unfollow' locale keys for English and Portuguese.

--- app/helpers/social/profiles_helper.rb
+++ app/helpers/social/profiles_helper.rb
@@ -5,7 +5,7 @@ module Social::ProfilesHelper
     unless Current.user.following?(profile)
       button_to t('follow'), follow_social_profile_path(profile), method: :post, class: "btn btn-success flex-shrink-0"
     else
-      button_to t('unfollow'), unfollow_social_profile_path(profile), method: :delete, class: "btn btn-danger flex-shrink-0"
+      button_to t('unfollow'), unfollow_social_profile_path(profile), method: :delete, class: "btn btn-secondary flex-shrink-0"
     end
   end
 end
