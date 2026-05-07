eceb6b6af159a709fda5b8f3934f80d2bf4a6136
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-05-07 12:46:18 -0300

    fix: upload preview for avatar and video forms

--- app/javascript/controllers/upload_preview_controller.js
+++ app/javascript/controllers/upload_preview_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["fileInput", "preview", "saveButton", "removeButton"]
+  static targets = ["fileInput", "preview", "saveButton", "removeButton", "clearButton"]
 
   preview() {
     const file = this.fileInputTarget.files[0]
@@ -10,11 +10,20 @@ export default class extends Controller {
     const reader = new FileReader()
     reader.onload = () => {
       this.previewTarget.src = reader.result
+      this.previewTarget.hidden = false
       this.showSaveButton()
 
       if (file.type.startsWith("video/")) this.videoPreview()
     }
     reader.readAsDataURL(file)
+  }
+
+  removePreview() {
+    this.fileInputTarget.value = ""
+    this.previewTarget.src = ""
+    this.previewTarget.hidden = true
+    if (this.hasClearButtonTarget) this.clearButtonTarget.hidden = true
+    if (this.hasRemoveButtonTarget) this.removeButtonTarget.hidden = false
   }
 
   videoPreview() {
@@ -25,5 +34,6 @@ export default class extends Controller {
   showSaveButton() {
     if (this.hasSaveButtonTarget) this.saveButtonTarget.hidden = false
     if (this.hasRemoveButtonTarget) this.removeButtonTarget.hidden = true
+    if (this.hasClearButtonTarget) this.clearButtonTarget.hidden = false
   }
 }
