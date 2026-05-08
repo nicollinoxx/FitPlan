83cc17c30c9f02765e5ccf425940518f1016a593
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-05-08 15:52:33 -0300

    Remove unused set_user before_action and method

--- app/controllers/identity/settings_controller.rb
+++ app/controllers/identity/settings_controller.rb
@@ -1,12 +1,4 @@
 class Identity::SettingsController < ApplicationController
-  before_action :set_user
-
   def show
   end
-
-  private
-
-    def set_user
-      @user = Current.user
-    end
 end
