f8d723b28b891aea946255dcceb1c5a2c6d5cbbc
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-05-10 17:03:22 -0300

    i18n/add-handle-translations
    - Add translations for handle-related features
    - Update schema documentation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_25_120000) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_10_193831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -155,6 +155,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_25_120000) do
     t.datetime "updated_at", null: false
     t.boolean "verified", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["handle"], name: "index_users_on_handle", unique: true
   end
 
   create_table "workouts", force: :cascade do |t|
