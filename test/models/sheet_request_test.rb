a7080be2c1618d072cfbaefbe91a896268a647af
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-05-14 13:41:08 -0300

    Refactor SheetRequest tests and fixtures

--- test/models/sheet_request_test.rb
+++ test/models/sheet_request_test.rb
@@ -2,85 +2,40 @@ require "test_helper"
 
 class SheetRequestTest < ActiveSupport::TestCase
   setup do
-    @sender = users(:lazaro_nixon)
+    @sender    = users(:lazaro_nixon)
     @recipient = users(:lazaro)
-    @sheet = sheets(:one)
-    @sheet_request = sheet_requests(:one)
+    @request   = sheet_requests(:one)
   end
 
-  teardown do
-    Current.reset
+  teardown { Current.reset }
+
+  test "sender? returns true for sender" do
+    assert @request.sender?(@sender)
   end
 
-  test "sender? should return true when user is sender" do
-    assert @sheet_request.sender?(@sender)
+  test "sender? returns false for recipient" do
+    refute @request.sender?(@recipient)
   end
 
-  test "sender? should return false when user is not sender" do
-    refute @sheet_request.sender?(@recipient)
+  test "receiver? returns true for recipient" do
+    assert @request.receiver?(@recipient)
   end
 
-  test "receiver? should return true when user is recipient" do
-    assert @sheet_request.receiver?(@recipient)
+  test "receiver? returns false for sender" do
+    refute @request.receiver?(@sender)
   end
 
-  test "receiver? should return false when user is not recipient" do
-    refute @sheet_request.receiver?(@sender)
-  end
-
-  test "create_for_sheets does nothing when more than 5 sheet_ids are sent" do
-    sheet_id = @sender.sheets.pick(:id)
-
-    assert_not_nil sheet_id
-    sheet_ids = Array.new(6, sheet_id)
-
-    assert_no_difference("SheetRequest.count") do
-      SheetRequest.create_for_sheets(sender: @sender, recipient: @recipient, sheet_ids: sheet_ids)
-    end
-  end
-
-  test "create_for_sheets does nothing when sheet_ids is empty" do
-    assert_no_difference("SheetRequest.count") do
-      SheetRequest.create_for_sheets(sender: @sender, recipient: @recipient, sheet_ids: [])
-    end
-  end
-
-  test "create_for_sheets creates pending requests for up to 5 valid sheet_ids" do
-    sheet_ids = [sheets(:two).id, sheets(:empty_workout).id]
-
-    assert_difference("SheetRequest.count", 2) do
-      SheetRequest.create_for_sheets(sender: @sender, recipient: @recipient, sheet_ids: sheet_ids)
-    end
-
-    created_requests = SheetRequest.where(sender: @sender, recipient: @recipient, sheet_id: sheet_ids)
-    assert_equal 2, created_requests.count
-    assert_equal ["pending"], created_requests.distinct.pluck(:status)
-  end
-
-  test "create_for_sheets raises RecordNotFound when a sheet does not belong to sender" do
-    outsider_sheet = @recipient.sheets.create!(name: "Recipient Sheet", sheet_type: "workout")
-
-    assert_no_difference("SheetRequest.count") do
-      assert_raises(ActiveRecord::RecordNotFound) do
-        SheetRequest.create_for_sheets(sender: @sender, recipient: @recipient, sheet_ids: [outsider_sheet.id])
-      end
-    end
-  end
-
-  test "cannot accept request when current user is not recipient" do
+  test "cannot accept when current user is not recipient" do
     Current.session = sessions(:sender_session)
 
-    error = assert_raises(ActiveRecord::RecordInvalid) do
-      @sheet_request.accepted!
-    end
-
+    error = assert_raises(ActiveRecord::RecordInvalid) { @request.accepted! }
     assert_includes error.record.errors[:base], I18n.t("errors.sheet_request.not_recipient")
   end
 
-  test "can accept request when current user is recipient" do
+  test "can accept when current user is recipient" do
     Current.session = sessions(:recipient_session)
 
-    @sheet_request.accepted!
-    assert @sheet_request.accepted?
+    @request.accepted!
+    assert @request.accepted?
   end
 end
