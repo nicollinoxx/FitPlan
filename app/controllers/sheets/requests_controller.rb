dfed20d69f8144f905203bd5ce1546fe93155b8d
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-05-14 12:42:28 -0300

    Extract preview partials for workouts and diets
    Move inline workout and diet preview markup into _workouts and _diets
    partials and update preview_content to render them. Add sheet_content
    helper to Sheets::RequestsController and remove the now-unused
    sheets/requests/show.html.erb. Also apply minor formatting tweaks in
    shares partials and turbo_stream templates.

--- app/controllers/sheets/requests_controller.rb
+++ app/controllers/sheets/requests_controller.rb
@@ -25,4 +25,8 @@ class Sheets::RequestsController < ApplicationController
     def set_request
       @request = SheetRequest.accessible_by(Current.user).find(params[:id])
     end
+
+    def sheet_content
+      @sheet.workout? ? @workouts : @diets
+    end
 end
