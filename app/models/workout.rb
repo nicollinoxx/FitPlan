e4664a9508df576bc9b105229003a4a2bcb511e4
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-05-27 10:34:46 -0300

    fix: add custom video size validation (max 16MB)

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -5,4 +5,14 @@ class Workout < ApplicationRecord
   has_many :completions, dependent: :destroy
 
   validates :exercise, :series, :repetitions, presence: true
+
+  validate :video_size
+
+  private
+
+  def video_size
+    return unless video.attached? && video.blob.byte_size > 16.megabytes
+
+    errors.add(:video, :too_large, max: "16MB")
+  end
 end
